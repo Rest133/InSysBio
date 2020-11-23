4d15209241471f3164c9aa000e86bebdeb81daf3
Rest133 <dubrovinanatolii@mail.ru>
2020-11-23 22:54:39 +0300

    End first page layout PC

---
+++ assets/js/slick_options.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 2,
+        infinite: false,
+        autoplay:false,
+        touchThreshold: 20,
+        variableWidth: true
+    });
+})
+
+$(document).ready(function () {
+    $('.news-slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 2,
+        infinite: false,
+        autoplay:false,
+        touchThreshold: 20,
+        variableWidth: true
+    });
+})
